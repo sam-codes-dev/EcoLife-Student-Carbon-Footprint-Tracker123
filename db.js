d68d85a16e37c178efd6cb8ffdba0c8f878cdb68
sam <samir2007412@gmail.com>
2026-01-16 10:03:56 +0530

    Update db.js

--- db.js
+++ db.js
@@ -1,12 +1,1 @@
--- database.sql
-CREATE DATABASE ecolife;
 
-USE ecolife;
-
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);

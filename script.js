10235df8b63a170d8b35035ae4ddb5870961671b
sam <samir2007412@gmail.com>
2026-01-16 09:55:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -194,3 +194,31 @@ document.getElementById("calcMoneyBtn")?.addEventListener("click",()=>{
         });
     }
 });
+async function signupUser() {
+    const name = document.getElementById('signupName').value;
+    const email = document.getElementById('signupEmail').value;
+    const password = document.getElementById('signupPassword').value;
+
+    const res = await fetch('http://localhost:3000/signup', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name, email, password })
+    });
+
+    const text = await res.text();
+    alert(text);
+}
+
+async function loginUser() {
+    const email = document.getElementById('loginEmail').value;
+    const password = document.getElementById('loginPassword').value;
+
+    const res = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ email, password })
+    });
+
+    const text = await res.text();
+    alert(text);
+}
